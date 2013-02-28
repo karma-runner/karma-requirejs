f210c2f541bcd3d2e6be197bdc5b73452570f080
Vojta Jina <vojta.jina@gmail.com>
2013-02-27 22:52:23 -0800

    Cherry-pick https://github.com/vojtajina/testacular/commit/59dbdbd136baa87467b9b9a4cb6ce226ae87bbef

--- src/adapter.js
+++ src/adapter.js
@@ -5,7 +5,13 @@
 var load_original = window.requirejs.load;
 requirejs.load = function (context, moduleName, url) {
   // TODO(vojta): should not append timestamp when debug.html
-  return load_original.call(this, context, moduleName, url + '?' + __testacular__.files[url]);
+  if (__testacular__.files.hasOwnProperty(url)) {
+    url = url + '?' + __testacular__.files[url];
+  } else {
+    console.error('There is no timestamp for ' + url + '!');
+  }
+
+  return load_original.call(this, context, moduleName, url);
 };
 
 require.config({
